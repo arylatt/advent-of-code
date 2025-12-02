cd737dc0925cbe8594c5298be1871d82f6c649ba
Andrew Rylatt <andrew+github@rylatt.me>
2025-12-02 08:25:23 +0000

    wip: 2025-02

---
+++ 2025/02/02.go
@@ -0,0 +1,86 @@
+package aoc202502
+
+import (
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/arylatt/advent-of-code/elves"
+)
+
+func parseInput(input string) [][2]string {
+	output := [][2]string{}
+
+	lines := elves.SplitIntoLines(input)
+
+	for _, vals := range strings.Split(lines[0], ",") {
+		pair := strings.Split(vals, "-")
+
+		output = append(output, [2]string{pair[0], pair[1]})
+	}
+
+	return output
+}
+
+func isInvalidId(id string) bool {
+	if len(id)%2 != 0 {
+		return false
+	}
+
+	mid := len(id) / 2
+	left := id[:mid]
+	right := id[mid:]
+
+	return left == right
+}
+
+func calculateNextInvalidId(id string) string {
+	if len(id)%2 != 0 {
+		id = strconv.Itoa(int(math.Pow(10, float64(len(id)))))
+
+		mid := len(id) / 2
+		return id[:mid] + id[:mid]
+	}
+
+	mid := len(id) / 2
+	left := id[:mid]
+
+	if (left + left) == id {
+		leftInt, _ := strconv.Atoi(left)
+		leftInt++
+		left = strconv.Itoa(leftInt)
+	}
+
+	return left + left
+}
+
+func Part1(input string) (output string) {
+	inputs := parseInput(input)
+	answer := 0
+
+	for _, pair := range inputs {
+		upperLimit := elves.Atoi(pair[1])
+		nextInvalid := elves.Atoi(calculateNextInvalidId(pair[0]))
+
+		if isInvalidId(pair[0]) {
+			answer += elves.Atoi(pair[0])
+		}
+
+		for nextInvalid <= upperLimit {
+			answer += nextInvalid
+
+			nextVal := nextInvalid + 1
+			if nextVal > upperLimit {
+				break
+			}
+
+			nextInvalid = elves.Atoi(calculateNextInvalidId(strconv.Itoa(nextVal)))
+		}
+	}
+
+	return strconv.Itoa(answer)
+}
+
+func Part2(input string) (output string) {
+	return
+}
---
+++ 2025/02/02_test.go
@@ -0,0 +1,56 @@
+package aoc202502
+
+import (
+	"testing"
+
+	"github.com/arylatt/advent-of-code/elves"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	// Sample1Answer is the expected answer for sample 1
+	Sample1Answer = "1227775554"
+
+	// Sample2Answer is the expected answer for sample 2
+	Sample2Answer = "Fill_In_Sample_2_Answer"
+)
+
+func TestCalculateNextInvalidId(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"10", "11"},
+		{"95", "99"},
+		{"998", "1010"},
+	}
+
+	for _, test := range tests {
+		result := calculateNextInvalidId(test.input)
+		assert.Equal(t, test.expected, result)
+	}
+}
+
+func TestPart1Sample(t *testing.T) {
+	td, err := elves.SampleFileToTestData(Sample1Answer)
+
+	if assert.NoError(t, err) {
+		elves.TestSample(t, td, Part1)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	elves.TestReal(t, Part1, 1, "2025", "2")
+}
+
+func TestPart2Sample(t *testing.T) {
+	td, err := elves.SampleFileToTestData(Sample2Answer)
+
+	if assert.NoError(t, err) {
+		elves.TestSample(t, td, Part2)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	elves.TestReal(t, Part2, 2, "2025", "2")
+}
